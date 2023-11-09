40120da0b228f26255f29c97a09c990c36bd0934
MuhammadRasul1 <elyorjonxxxxx@gmail.com>
2023-11-09 17:05:44 +0500

    commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector("site-header").classList.toggle("open-btn")
+    })    
+})
+
